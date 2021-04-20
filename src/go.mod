59e20603434ac8e44db75e54c38bf0213bc9974d
Nikolay Kiselev <55307887+kiselev-nikolay@users.noreply.github.com>
2021-04-20 15:04:46 +0300

    Init go

---
+++ src/go.mod
@@ -0,0 +1,5 @@
+module github.com/kiselev-nikolay/hackaton210419-prerstroika/src
+
+go 1.16
+
+require github.com/jackc/pgx/v4 v4.11.0 // indirect
---
+++ src/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
